c39b740820b98cb24d730ac971bf3d3b68677538
Spence Konde (aka Dr. Azzy) <spencekonde@gmail.com>
2018-02-28 17:32:41 -0500

    Update DriftAnimatePlus.ino

--- DriftAnimatePlus/DriftAnimatePlus.ino
+++ DriftAnimatePlus/DriftAnimatePlus.ino
@@ -1,9 +1,21 @@
 #include <Adafruit_NeoPixel_Static.h>
+#include <Wire.h>
+
 
 void setup() {
 
 }
 
 void loop() {
-
+  byte rlen = handleReceive();
+  if (rlen) {
+    outputPacket(rlen);
+  } else if (!rxing) {
+    handleUI();
+    handleLCD();
+    if (millis() - lastFrameAt > frameDelay) {
+      lastFrameAt=millis();
+      updatePattern();
+      leds.show();
+  }
 }
