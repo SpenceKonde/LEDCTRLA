6d1d1266239aadc6784296cd331e92d730db8c43
Spence Konde (aka Dr. Azzy) <spencekonde@gmail.com>
2018-03-07 16:28:39 -0500

    Update DriftAnimatePlus.ino

--- DriftAnimatePlus/DriftAnimatePlus.ino
+++ DriftAnimatePlus/DriftAnimatePlus.ino
@@ -1,6 +1,26 @@
 #include <Adafruit_NeoPixel_Static.h>
 #include <Wire.h>
 
+// UI + encoder involved globals 
+volatile byte lastEncPins=0;
+volatile byte currentSettingLeft=0;
+volatile byte currentSettingRight=0;
+
+byte currentMode=0;
+byte currentSetting=0;
+
+volatile unsigned long lastUserAction=0;
+
+
+//animation related globals
+#define LENGTH 200;
+
+unsigned long lastFrameAt
+byte pixels[LENGTH*3];
+byte target[LENGTH*3];
+byte param[LENGTH];
+
+
 
 void setup() {
 
