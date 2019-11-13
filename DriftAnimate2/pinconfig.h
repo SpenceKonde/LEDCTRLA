8e2096141360e5e4f2fe551ae4340eec4a7462a7
Spence Konde (aka Dr. Azzy) <spencekonde@gmail.com>
2019-11-13 12:44:05 -0500

    Add setupPins and setupPCINT and backlight/LCD setup functions

--- DriftAnimate2/pinconfig.h
+++ DriftAnimate2/pinconfig.h
@@ -1,4 +1,5 @@
 // UI + encoder involved globals
+// General functions that vary based on what hardware is in use
 
 #ifndef __AVR_ATmega1284P__
 #ifdef __AVR_ATmega328PB__
@@ -11,8 +12,11 @@ LiquidCrystal lcd(5, 6, 7, 23, 24, 25, 26);
 #define LEDPIN 9
 #include <LiquidCrystal_I2C.h>
 #include <Wire.h>
+#ifdef RED_DOT
 LiquidCrystal_I2C lcd(0x3F, 16, 2); //red dot
-//LiquidCrystal_I2C lcd(0x27, 16, 2); //green dot
+#else
+LiquidCrystal_I2C lcd(0x27, 16, 2); //green dot
+#endif
 #define LDR_PIN A6
 #endif
 #define RX_PIN_STATE (PINB&1) //RX on pin 8 for input capture. 
@@ -37,3 +41,63 @@ LiquidCrystal lcd(2, 3, 4, 24, 25, 26, 27);
 #define ENC2_BTN 11
 #define MODE_BTN 10
 #endif
+
+void setupLCD() {
+  #ifdef __AVR_ATmega328P__
+  Wire.begin();
+  lcd.begin();
+  #else 
+  lcd.begin(16,2);
+  #endif
+}
+
+void backlightOn() {
+  #ifdef __AVR_ATmega328P__
+  lcd.backlight();
+  #else
+  #ifndef __AVR_ATmega1284P__
+  pinMode(18,OUTPUT);
+  digitalWrite(18,1);
+  #else
+  pinMode(20,OUTPUT);
+  digitalWrite(20,1);
+  #endif
+  #endif
+}
+
+void backlightOff() {
+    lcd.noBacklight();
+  #else
+  #ifndef __AVR_ATmega1284P__
+  digitalWrite(18,0);
+  #else
+  digitalWrite(20,0);
+  #endif
+  #endif
+}
+
+void setupPins() {
+  pinMode(LEDPIN, OUTPUT);
+  pinMode(ENC1_PINA, INPUT_PULLUP);
+  pinMode(ENC1_PINB, INPUT_PULLUP);
+  pinMode(ENC1_BTN, INPUT_PULLUP);
+  pinMode(ENC2_PINA, INPUT_PULLUP);
+  pinMode(ENC2_PINB, INPUT_PULLUP);
+  pinMode(ENC2_BTN, INPUT_PULLUP);
+  pinMode(MODE_BTN, INPUT_PULLUP);
+  pinMode(A4, INPUT_PULLUP);
+  pinMode(A5, INPUT_PULLUP);
+}
+
+void setupPCINT() {
+#ifndef __AVR_ATmega1284P__ //encoders are PCINT8~15 range on 328p
+  PCMSK1 = 0x0F;
+  PCICR = 2;
+#else //encoders are PCINT16~24 range on 1284p
+  PCMSK2 = 0x0F;
+  PCICR = 4;
+#endif
+}
+
+
+
