19ad94d3adb11678154fe40aa40236acd300f58d
Spence Konde (aka Dr. Azzy) <spencekonde@gmail.com>
2019-11-13 16:38:49 -0500

    Update AzzyRFRX.h

---
+++ DriftAnimate2/AzzyRFRX.h
@@ -0,0 +1,166 @@
+#include <util/crc16.h>
+#include "pinconfig.h"
+
+//RF related globals
+const byte MyAddress = 0;
+volatile byte receiving = 0;
+volatile byte bitnum = 0; //current bit received
+
+volatile byte gotMessage = 0;
+volatile byte dataIn = 0;
+volatile byte pktLength = 31;
+volatile unsigned long lastRFMsgAt = 0;
+volatile byte rxBuffer[32];
+byte recvMessage[32];
+
+unsigned long lastPacketTime = 0;
+unsigned long lastPacketSig = 0;
+
+// Version 2.2/2.3
+#if(F_CPU==8000000)
+#define TIME_MULT * 1
+#elif(F_CPU==16000000)
+#define TIME_MULT * 2
+#endif
+
+const unsigned int rxSyncMin  = 1750 TIME_MULT;
+const unsigned int rxSyncMax  = 2250 TIME_MULT;
+const unsigned int rxZeroMin  = 100 TIME_MULT;
+const unsigned int rxZeroMax  = 390 TIME_MULT;
+const unsigned int rxOneMin  = 410 TIME_MULT;
+const unsigned int rxOneMax  = 700 TIME_MULT;
+const unsigned int rxLowMax  = 600 TIME_MULT;
+const int commandForgetTime = 5000;
+
+byte handleReceive() {
+  if (gotMessage) {
+    byte vers = checkCSC(); //checkCSC() gives 0 on failed CSC, 1 on v1 structure (ACD...), 2 on v2 structure (DSCD...)
+    if (!vers) { //if vers=0, unknown format ot bad CSC
+      resetReceive();
+      return 0;
+    }
+    if (!isForMe()) { //matches on MyAddress==0, destination address==0, destination address==MyAddress.
+      resetReceive();
+      return 0;
+    }
+    if (lastPacketSig == getPacketSig() && lastPacketTime) {
+
+      lastPacketTime = millis();
+      resetReceive();
+      return 0;
+    }
+    lastPacketSig = getPacketSig();
+    lastPacketTime = millis();
+    byte rlen = ((pktLength >> 3) + 1) | ((vers - 1) << 6);
+
+    memcpy((void*)recvMessage, (const void*)rxBuffer, 32); //copy received message - safe because we haven't called resetReceive, so IC int is off. 
+    resetReceive();
+    return rlen;
+  } else {
+    unsigned long t = (millis() - lastPacketTime);
+    if (lastPacketTime && (t > commandForgetTime)) {
+      lastPacketTime = 0;
+      lastPacketSig = 0;
+    }
+    return 0;
+  }
+}
+
+void resetReceive() {
+  if (bitnum > 4) {
+    lastRFMsgAt = millis();
+  }
+  bitnum = 0;
+  memset((void*)rxBuffer, 0, 32); //clear buffer - safe because haven't reenabled IC interrupt yet. 
+  gotMessage = 0;
+  TIMSK1 = 1 << ICIE1;
+  return;
+}
+
+byte checkCSC() {
+  byte rxchecksum = 0;
+  byte rxchecksum2 = 0;
+  for (byte i = 0; i < pktLength >> 3; i++) {
+    rxchecksum = rxchecksum ^ rxBuffer[i];
+    rxchecksum2 = _crc8_ccitt_update(rxchecksum2, rxBuffer[i]);
+  }
+  if (pktLength >> 3 == 3) {
+    rxchecksum = (rxchecksum & 0x0F) ^ (rxchecksum >> 4) ^ ((rxBuffer[3] & 0xF0) >> 4);
+    rxchecksum2 = (rxchecksum2 & 0x0F) ^ (rxchecksum2 >> 4) ^ ((rxBuffer[3] & 0xF0) >> 4);
+    if (rxchecksum == rxchecksum2)rxchecksum2++;
+    return (rxBuffer[3] & 0x0F) == rxchecksum ? 1 : ((rxBuffer[3] & 0x0F) == rxchecksum2 ) ? 2 : 0;
+  } else {
+    if (rxchecksum == rxchecksum2)rxchecksum2++;
+    return ((rxBuffer[pktLength >> 3] == rxchecksum) ? 1 : ((rxBuffer[pktLength >> 3] == rxchecksum2 ) ? 2 : 0));
+  }
+}
+
+byte isForMe() {
+  if ((rxBuffer[0] & 0x3F) == MyAddress || MyAddress == 0 || (rxBuffer[0] & 0x3F) == 0) {
+    return 1;
+  }
+  return 0;
+}
+
+unsigned long getPacketSig() {
+  byte len = pktLength >> 3;
+  unsigned long lastpacketsig = 0;
+  for (byte i = (len == 3 ? 0 : 1); i < (len == 3 ? 3 : 4); i++) {
+    lastpacketsig += rxBuffer[i];
+    lastpacketsig = lastpacketsig << 8;
+  }
+  lastpacketsig += rxBuffer[len];
+  return lastpacketsig;
+}
+
+ISR (TIMER1_CAPT_vect)
+{
+  static unsigned long lasttime = 0;
+  unsigned int newTime = ICR1; //immediately get the ICR value
+  byte state = (RX_PIN_STATE);
+  TCCR1B = state ? (1 << CS11 | 1 << ICNC1) : (1 << CS11 | 1 << ICNC1 | 1 << ICES1); //and set edge
+  unsigned int duration = newTime - lasttime;
+  lasttime = newTime;
+  if (state) {
+    if (receiving) {
+      if (duration > rxLowMax) {
+        receiving = 0;
+        bitnum = 0; // reset to bit zero
+        memset((void*)rxBuffer, 0, 32); //clear buffer - memset on rxBuffer is safe because interrupts disabled in ISR
+      }
+    } else {
+      if (duration > rxSyncMin && duration < rxSyncMax) {
+        receiving = 1;
+      }
+    }
+  } else {
+    if (receiving) {
+      if (duration > rxZeroMin && duration < rxZeroMax) {
+        dataIn = dataIn << 1;
+      } else if (duration > rxOneMin && duration < rxOneMax) {
+        dataIn = (dataIn << 1) + 1;
+      } else {
+        receiving = 0;
+        bitnum = 0; // reset to bit zero
+        memset((void*)rxBuffer, 0, 32); //clear buffer - memset on rxBuffer is safe because interrupts disabled in ISR
+        return;
+      }
+      if ((bitnum & 7) == 7) {
+        rxBuffer[bitnum >> 3] = dataIn;
+        if (bitnum == 7) {
+          byte t = dataIn >> 6;
+          pktLength = t ? (t == 1 ? 63 : (t == 2 ? 127 : 255)) : 31;
+        }
+        dataIn = 0;
+      }
+      if (bitnum >= pktLength) {
+        bitnum = 0;
+        receiving = 0;
+        gotMessage = 1;
+        TIMSK1 = 0; //turn off input capture;
+      } else {
+        bitnum++;
+      }
+    }
+  }
+}
