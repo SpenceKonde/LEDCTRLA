6affb8f9ddb43d1f087ece565ab1bb89ef4b0279
Spence Konde <spencekonde@gmail.com>
2022-08-01 02:01:28 -0400

    big changs

---
+++ DriftAnimate_V21/lightsting.cpp
@@ -0,0 +1,8 @@
+//we detect length with a passive resistor between ground and ID pin. These are the key values that serve as divisions between them
+
+uint16_t length_buckets = [0, 344, 983, 1471, 1877, 2173, 2328, 2503, 2675, 2808, 2988, 3272, 3495, 3716, 3971, 4096];
+
+typedef struct Light_t {
+  int16_t length;
+  int
+} lights
---
+++ DriftAnimate_V21/lightstrings.h
@@ -0,0 +1,41 @@
+uint16_t values = 1444,1855,2152,2329,2586,2938,3254,3480,3705,3966
+
+
+const uint16_t buckets[12] = {0,23107,29677,34428,37264,41369,47007,52058,55686,59276,63453,65520};
+//                             0 |  one | two |three| four| five| six |seven|eight|nine | ten |
+//uint16_t buckets[14] = {0,23106,29677,34427,37263,39961,42695,46225,49129,52057,55686,59276,63452,65535}
+/*
+Buckets:
+Less   | Short to ground -> it's a string of non-split lights
+22k    | 50 led string GRB
+33k    | 100 led string GHB
+39k    | 200 led string GRB
+47k    | 50 led string RGB
+68k    | 100 led string RGB
+100k   | 200 led string RGB
+150k   | Reserved
+220k   | Reserved.
+470k   | 332 LED strip CoB
+more   | Open -> nothing connected to this pin!
+
+
+#include <TypeUnions.h>
+
+byteWordLong voltage;
+
+
+uint8_t findBucket(pin) {
+  voltage.l = analogReadEnh(pin,16);
+  if (voltage.b[3] == != 0) { //There is never a correct time for any value this large or small to come from analogReadEnh().
+    //ADC error?!
+  } else {
+    byte bucket = 0;
+    while (bucket < 11) {
+      if (buckets[bucket] =< voltage.i && buckets[bucket+1] >= voltage.i) {
+        break;
+      }
+      bucket++;
+    }
+  }
+  return bucket;
+}
